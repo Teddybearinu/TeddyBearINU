f102b87d04daeb05da4f26e1bb7ec9c6dc63924e
XFantasy88 <erencote@gmail.com>
2024-03-17 11:12:47 -0400

    update

--- teddy-website/src/app/page.tsx
+++ teddy-website/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         <TokenDetails />
         <Guide />
         <Swap />
-        <Team />
+        {/* <Team /> */}
         <Dapps />
         <Holdings />
         <Roadmap />
